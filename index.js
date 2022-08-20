2e61af9f5e89b1f82e535e81f4e24b280294a0ce
Jeanette Ralph <jean.ralph@hotmail.com>
2022-08-20 09:45:44 -0700

    fixed adding prompts

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(
         user: 'root',
 
         // MySQL password
-        password: 'password',
+        password: '',
         database: 'department_db'
     },
     console.log(`Connected to the department_db database.`)
@@ -126,18 +126,15 @@ const addDepartment = () => {
             name: 'deptName'
         }
     ]).then(res => {
-        // const deptName = res;
-        db.query(
-            'INSERT INTO department (department_name) VALUES (?)', deptName,
-            function (err, results) {
-                console.table(results);
-            }
-        )
-    }).then(() => promptMenu());
+        const { deptName } = res;
+        db.promise()
+            .query('INSERT INTO department (department_name) VALUES (?)', deptName)
+            .then(() => console.log(`Added ${deptName} to db`))
+    }).then(() => promptMenu())
 
 };
 
-const addRole = (response) => {
+const addRole = () => {
     console.log('Add a role.');
     inquirer.prompt([
         {
@@ -150,34 +147,42 @@ const addRole = (response) => {
             type: 'input',
             message: 'What is the salary of the role?',
             name: 'salary'
-        },
+        }]
+    ).then(res => {
+        // get the role and the salary the user has defined
+        const { role, salary } = res;
 
-        {
-            type: 'list',
-            message: 'Which deparment does the role belong to?',
-            choices: [
-                'Engineering',
-                'Finance',
-                'Legal',
-                'Sales',
-                'Service'
-            ],
-            name: 'deptRole'
-        },
-    ]).then(
-        db.query(
-            'INSERT INTO role (title, salary,department_id ) VALUES (?,?,?)', [response.role, response.salary, response.deptRole],
-            function (err, results) {
-                console.table(results);
-            }
-        )
-    );
+        // fetch all the departments from the db to assign the role to a department
+        db.promise().query('SELECT * FROM department')
+            .then(([rows]) => {
+                const allDepartments = rows;
 
-    promptMenu();
+                // create the choices from the departments
+                const departmentChoices = allDepartments.map(({ department_id, department_name }) => ({
+                    name: department_name,
+                    value: department_id
+                }));
+
+                // prompt the user to select a department and then create the role
+                inquirer.prompt({
+                    type: 'list',
+                    name: 'departmentId',
+                    message: 'What department does the role belong to?',
+                    choices: departmentChoices
+                }).then(response => {
+                    // create the new role with the role, salary, and departmentId
+                    console.log('response', response);
+                    db.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)', [role, salary, response.departmentId])
+                        .then(() => console.log('Role added successfully.'))
+                        .then(() => promptMenu());
+                })
+            })
+    })
 };
 
-const addEmployee = (response) => {
+const addEmployee = () => {
     console.log('Add an employee.');
+
     inquirer.prompt([
         {
             type: 'input',
@@ -189,41 +194,71 @@ const addEmployee = (response) => {
             type: 'input',
             message: "What is the employee's last name?",
             name: 'lastName'
-        },
+        }]).then(res => {
+            // get the first name and the last name the user has defined
+            const { firstName, lastName } = res;
 
-        {
-            type: 'list',
-            message: "What is the employee's role?",
-            choices: [
-                'Sales Person',
-                'Software Engineer',
-                'Account Manager',
-                'Accountant',
-                'Project Manager',
-                'Lawyer'
-            ],
-            name: 'employeeRole'
-        },
+            // fetch all the roles from the db to assign the new employee to a role
+            db.promise().query('SELECT * FROM role')
+                .then(([rows]) => {
+                    const allRoles = rows;
 
-        {
-            type: 'list',
-            message: "Who is the employee's manager?",
-            choices: [
-                'Nisha Singh',
-                'Harpreet Kaur'
-            ],
-            name: 'employeeManager'
+                    // create the choices from the roles
+                    const roleChoices = allRoles.map(({ role_id, title }) => ({
+                        name: title,
+                        value: role_id
+                    }));
+
+                    // prompt the user to select a role the employee belongs to
+                    inquirer.prompt({
+                        type: 'list',
+                        name: 'roleId',
+                        message: "What is the employee's role?",
+                        choices: roleChoices
+                    }).then(response => {
+                        // create the new role with the role, salary, and departmentId
+                        console.log('response', response);
+                        db.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)', [role, salary, response.departmentId])
+                            .then(() => console.log('Role added successfully.'))
+                            .then(() => promptMenu());
+                    })
+                })
         }
-    ]).then(
-        db.query(
-            'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?)', [response.firstName, response.lastName, response.employeeRole, employeeManager],
-            function (err, results) {
-                console.table(results);
-            }
-        )
-    );
 
-    promptMenu();
+
+            // create this dynamically
+            {
+                type: 'list',
+                message: "What is the employee's role?",
+                choices: allRoles,
+                // choices: [
+                //     'Sales Person',
+                //     'Software Engineer',
+                //     'Account Manager',
+                //     'Accountant',
+                //     'Project Manager',
+                //     'Lawyer'
+                // ],
+                name: 'employeeRole'
+            },
+
+        // create this dynamically
+        // {
+        //     type: 'list',
+        //     message: "Who is the employee's manager?",
+        //     choices: [
+        //         'Nisha Singh',
+        //         'Harpreet Kaur'
+        //     ],
+        //     name: 'employeeManager'
+        // }
+    ]).then(res => {
+                console.log('RES: ', res);
+                db.query(
+                    'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?)', [res.firstName, res.lastName, res.employeeRole, res.employeeManager],
+                )
+            }
+            ).then(() => promptMenu())
 };
 
 // for updating values
@@ -260,15 +295,12 @@ const updateEmployee = () => {
                 name: 'newEmployeeRole'
             }
         ]
-    ).then(
+    ).then(res => {
         db.query(
-            'UPDATE employee SET first_name = ? WHERE employee_id = ? ', [response.employeeUpdate, response.newEmployeeRole],
-            function (err, results) {
-                console.table(results);
-            }
-        )
-    )
+            'UPDATE employee SET first_name = ? WHERE employee_id = ? ', [res.employeeUpdate, res.newEmployeeRole],
 
-    promptMenu();
+        )
+    }
+    ).then(() => promptMenu())
 };
 
